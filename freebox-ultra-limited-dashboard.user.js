39ea4655c445cc22cae3b83b188ead583299ba29
Steven17200 <stephane.denizault@gmail.com>
2026-01-01 06:21:06 +0100

    Update freebox-ultra-limited-dashboard.user.js

--- freebox-ultra-limited-dashboard.user.js
+++ freebox-ultra-limited-dashboard.user.js
@@ -17,7 +17,7 @@
 (function() {
     'use strict';
  // --- Changer la source de l'image par le votre ---
-    const boxImageUrl = "https://i.postimg.cc/gkhT4nRt/freebox-limited.png";
+    const boxImageUrl = "https://github.com/Steven17200/freebox-os-ultra-dashboard/blob/main/freebox%20limited.png?raw=true";
 
     function getTempColor(temp) {
         if (temp < 63) return '#4CAF50';
